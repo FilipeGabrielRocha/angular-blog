4f75f20207c8982fac420157a06dd1d0ea78e2b2
FilipeGabrielRocha <filiperochaprogramador@gmail.com>
2024-02-19 15:24:02 -0300

    Terminando o projeto do blog angular

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -1,26 +1,34 @@
 export const dataFake = [
   {
-    "id": 1,
-    "photo": "",
-    "title": "",
-    "description": ""
+    "id": '1',
+    "trailerGame": "vL44VbiSnpY?si=ITf9C-UsbaRCVPA9",
+    "name": "Diablo 4" ,
+    "photoCover": "../../assets/img/diablo-iv.jpg",
+    "title": "Diablo 4: Novo jogo da Blizzard",
+    "description": "Diablo 4 é a mais nova sequência da aclamada franquia de RPG de ação da Blizzard Entertainment. Sua base se assemelha bastante aos outros títulos da franquia, com destaque para o bom número de dungeons e diferentes classes disponíveis. Porém, o game apresenta alguns elementos novos e uma evolução gráfica perceptível."
   },
   {
-    "id": 2,
-    "photo": "",
-    "title": "",
-    "description": ""
+    "id": '2',
+    "trailerGame": "PWe0pC2akpo?si=bgtoXGKfY_vaSz75",
+    "name": "Overwatch 2",
+    "photoCover": "../../assets/img/overwatch-2.webp",
+    "title": "Lançamento Overwatch 2",
+    "description": "Notícia boa para os fãs de Overwatch 2, a partir de 10 de agosto, o jogo estará disponível no Steam. Esse é o primeiro de uma seleção de jogos da Blizzard que entrarão na plataforma. De acordo com a empresa, essa é uma oportunidade de diminuir as barreiras e tornar mais fácil para os jogadores de todo o mundo aproveitarem os jogos da publisher. Atualmente, a Battle.net é o lar de todos os jogadores de PC da Blizzard. No entanto, ainda que a empresa tenha desenvolvido a plataforma para apoiar as tecnologias atualizadas e expectativas da comunidade, ela entende que indústria evoluiu e os jogos não são mais apenas para comunidades específicas. Mesmo assim, a Blizzard continuará comprometida a investir e apoiar a plataforma que já existe há mais de duas décadas."
   },
   {
-    "id": 3,
-    "photo": "",
-    "title": "",
-    "description": ""
+    "id": '3',
+    "trailerGame": "PP7yRXdHAT0?si=-UI-GwAxwai4ZRAW",
+    "name": "Path Of Exile",
+    "photoCover": "../../assets/img/path-of-exile.jpg",
+    "title": "Nova temporada do jogo Path Of Exile",
+    "description": "O estúdio neozelandês Grinding Gear Games revelou a nova liga de Path of Exile, Affliction. O arranque desta nova temporada está marcado para o próximo dia 8 de dezembro no PC e dia 15 na PlayStation e Xbox. Com a nova liga, o GGG promete dar um forte abanão no meta do jogo, não só com a introdução de novo conteúdo, como também com uma reformulação profunda do sistema de habilidades do jogo."
   },
   {
-    "id": 4,
-    "photo": "",
-    "title": "",
-    "description": ""
-  },
+    "id": '4',
+    "trailerGame": "2UJnjYXu5x4?si=46yTivw1tpbYEhLQ",
+    "name": "PokeMMO",
+    "photoCover": "../../assets/img/pokemmo.png",
+    "title": "Servidor de Pokemon: PokeMMO",
+    "description": "Fãs de Pokémon agora podem experimentar um pouco da sensação de encontrar outros treinadores de monstrinhos por aí. Um grupo de fãs transformou “Pokémon Fire Red” em um game online, batizado de “PokeMMO”. “Pokemon Fire Red” foi lançado originalmente para Game Boy Advance em 2004 e é um remake da versão que saiu para o Game Boy em 1998. No “PokeMMO”, o gamer percorre toda a aventura do título original, mas em um servidor com outras pessoas jogando a mesma campanha."
+  }
 ]
--- src/app/pages/content/content.component.ts
+++ src/app/pages/content/content.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 import { ActivatedRoute } from '@angular/router';
+import { dataFake } from 'src/app/data/dataFake';
 
 @Component({
   selector: 'app-content',
@@ -10,18 +12,47 @@ import { ActivatedRoute } from '@angular/router';
   ]
 })
 export class ContentComponent implements OnInit {
+  name:string =""
   photoCover:string = ""
   contentTitle:string = ""
   contentDescription:string = ""
+  trailerGame:string = ""
+  private id:string | null = "0"
+  safeTrailerGameUrl: SafeResourceUrl = ""
 
   constructor(
-    private route:ActivatedRoute
+    private route:ActivatedRoute,
+    private sanitizer: DomSanitizer,
   ) { }
 
   ngOnInit(): void {
     this.route.paramMap.subscribe( value =>
-      console.log(value.get("id"))
+      this.id = value.get("id")
     )
+
+    this.setValuesToComponent(this.id)
   }
+
+  setValuesToComponent(id:string | null){
+    const result = dataFake.filter(article => article.id == id)[0]
+
+    this.photoCover = result.photoCover
+    this.contentTitle = result.title
+    this.contentDescription = result.description
+    this.name = result.name
+    this.trailerGame = result.trailerGame
+    this.safeTrailerGameUrl = this.getSafeVideoUrl(this.trailerGame)
+
+    console.log(this.trailerGame);
+    console.log(this.safeTrailerGameUrl);
+
+  }
+
+  getSafeVideoUrl(videoId: string): SafeResourceUrl {
+    const videoUrl = `https://www.youtube-nocookie.com/embed/${videoId}`;
+    return this.sanitizer.bypassSecurityTrustResourceUrl(videoUrl);
+  }
+
+
 
 }
