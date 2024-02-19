fc5d21c8296f4800b58913af251a9b92f9af9947
FilipeGabrielRocha <filiperochaprogramador@gmail.com>
2024-02-19 14:00:56 -0300

    Pegando os dados dinâmicamente

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -4,5 +4,23 @@ export const dataFake = [
     "photo": "",
     "title": "",
     "description": ""
-  }
+  },
+  {
+    "id": 2,
+    "photo": "",
+    "title": "",
+    "description": ""
+  },
+  {
+    "id": 3,
+    "photo": "",
+    "title": "",
+    "description": ""
+  },
+  {
+    "id": 4,
+    "photo": "",
+    "title": "",
+    "description": ""
+  },
 ]
