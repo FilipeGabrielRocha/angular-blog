517ac1d1af28c3d3ef43887f20605493571cce3e
FilipeGabrielRocha <filiperochaprogramador@gmail.com>
2024-02-19 13:34:19 -0300

    Criando a página content

---
+++ src/app/components/big-card/big-card.component.ts
@@ -0,0 +1,22 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-big-card',
+  templateUrl: './big-card.component.html',
+  styleUrls: ['./big-card.component.css']
+})
+export class BigCardComponent implements OnInit {
+  descriptionPhoto:string = "Fevereiro 19, 2024"
+  @Input()
+  photoCover: string = ""
+  @Input()
+  cardTitle:string = ""
+  @Input()
+  cardDescription:string = ""
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
